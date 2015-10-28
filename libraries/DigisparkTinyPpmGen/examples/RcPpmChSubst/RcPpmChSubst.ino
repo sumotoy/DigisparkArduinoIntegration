0129d66700e622a88bb2405931157c9a8e021a6f
RC-Navy <p.loussouarn@free.fr>
2015-10-28 22:05:04 +0100

    RcRxPop.h, RcTxPop and Diagram added.

--- libraries/DigisparkTinyPpmGen/examples/RcPpmChSubst/RcPpmChSubst.ino
+++ libraries/DigisparkTinyPpmGen/examples/RcPpmChSubst/RcPpmChSubst.ino
@@ -2,6 +2,17 @@
 RcPpmChSubst sketch
 by RC Navy (http://p.loussouarn.free.fr/arduino/arduino.html) 2015
 This sketch reads an RC PPM frame, extracts the numbers of channels and their pulse witdhs and substitutes the 4th channel with a fix pulse width (2000 us).
+
+.----------------.   .---------------.
+| Trainer Output |-->| TinyPpmReader |
+|                |   '---------------'
+|                |         |
+| RC Transmitter |         | Forward channels 1 to 3 and set 4th channel width to 2000 us
+|                |         V
+|                |   .---------------.
+| Trainer Intput |<--|  TinyPpmGen   |
+'----------------'   '---------------'
+
 This sketch can work with a Digispark pro, Digispark and Arduino UNO.
 The PPM input shall support pin change interrupt.
 
@@ -24,21 +35,25 @@ This example code is in the public domain.
 */
 #include <TinyPinChange.h>
 #include <TinyPpmReader.h>
+#include <RcRxPop.h>
+
 #include <TinyPpmGen.h>
+#include <RcTxPop.h>
 
 #define PPM_INPUT_PIN  2
+#define CHANNEL_NB     4
 
 TinyPpmReader MyPpmReader;  /* Create a TinyPpmReader object */
 
 void setup()
 {
-  TinyPpmGen.begin(TINY_PPM_GEN_POS_MOD, 4); /* Change TINY_PPM_GEN_POS_MOD to TINY_PPM_GEN_NEG_MOD for NEGative PPM modulation */
   MyPpmReader.attach(PPM_INPUT_PIN); /* Attach MyPpmReader to PPM_INPUT_PIN pin */
+  TinyPpmGen.begin(TINY_PPM_GEN_POS_MOD, CHANNEL_NB); /* Change TINY_PPM_GEN_POS_MOD to TINY_PPM_GEN_NEG_MOD for NEGative PPM modulation */
 }
 
 void loop()
 {
-  if((MyPpmReader.detectedChannelNb() >= 4) && MyPpmReader.isSynchro())
+  if((MyPpmReader.detectedChannelNb() >= CHANNEL_NB) && MyPpmReader.isSynchro())
   {
     TinyPpmGen.setChWidth_us(1, MyPpmReader.width_us(1)); /* RC Channel#1: forward rx value */
     TinyPpmGen.setChWidth_us(2, MyPpmReader.width_us(2)); /* RC Channel#2: forward rx value */
